addd84ce3c458ff966de02c06d67d7f01e538adc
Isi <ioa213@nyu.edu>
2014-11-07 17:08:43 -0500

    Apparently works-for-me is a crappy excuse.

--- public/Javascripts/client.js
+++ public/Javascripts/client.js
@@ -1,24 +1,81 @@
 /**
  * Created by Isi on 11/5/14.
  */
-var socket = io.connect();
+var socket = io.connect('/');
 
 socket.on('connect', function (data){
     console.log('Connected');
 });
 
-//var m = document.getElementById('messages').innerHTML;
+socket.on('audio', function(data){
+    console.log("Got audio" + data.left.length);
+    var leftBuffer = new Float32Array(data.left);
+    var audioContext = new AudioContext();
+    var audioSource = audioContext.createBuffer(1, leftBuffer.length, audioContext.sampleRate);
+    newBuffer.getChannelData(0).set(leftBuffer);
 
-var sen = function(input){
-    sendMessage(document.getElementById('m').value);
-};
+    audioSource.buffer = newBuffer;
 
-socket.on('chat message', function(data){
-    document.getElementById('messages').innerHTML = " " + data + " <br> " +
-    document.getElementById('messages').innerHTML;
+    audioSource.connect( audioContext.destination );
+    audioSource.start(0);
 });
 
-var sendMessage = function (message) {
-    console.log("chat message: " + message);
-    socket.emit('chat message', message);
-};
+
+function init(){
+    window.URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+    if (navigator.getUserMedia) {
+        navigator.getUserMedia({audio: true}, function(stream) {
+
+                var audioContext = new AudioContext();
+                var audioSource = audioContext.createMediaStreamSource(stream);
+
+
+                //recorder
+                var recorder;
+                recorder = new Recorder(audioSource); // Once we have the stream...
+
+                startRec = function(button) {
+                    recorder.record();
+                    button.disabled = true;
+                    button.nextElementSibling.disabled = false;
+                    console.log('Recording...');
+                };
+
+                stopRec = function(button) {
+                    recorder.stop();
+                    button.disabled = true;
+                    button.previousElementSibling.disabled = false;
+                    console.log('Stopped recording.');
+
+                    recorder.getBuffer(function(buffers) {
+                        // Binary data - ArrayBuffer
+                        var bufferToSend = buffers[0].buffer;
+                        var dataToSend = {left: bufferToSend};
+                        socket.emit('audio',dataToSend);
+                    });
+                    createPlayer();
+
+                };//end of stopRecording
+
+                createPlayer = function(){
+
+                    recorder.exportWAV(function (blob) {
+                        var li = document.createElement('li');
+                        var au = document.createElement('audio');
+
+                        au.controls = true;
+                        li.appendChild(au);
+                        recordingslist.appendChild(li);
+
+
+                    })
+                }
+
+            }, function(error) {alert("Failure " + error.code);}
+        );
+    }//end of get user media
+}//end of init
+
+window.addEventListener('load', init);
